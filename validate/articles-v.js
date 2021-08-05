2d27575faf5b3e6259cfa8d5b60dbc0107fd9166
邓威 <1536698087@qq.com>
2021-08-05 21:14:27 +0800

    封装文章id校验中间件

--- validate/articles-v.js
+++ validate/articles-v.js
@@ -1,8 +1,17 @@
 const validate = require('../middleware/validate')
-const { body } = require('express-validator')
+const { body, param } = require('express-validator')
+const mongoose = require('mongoose')
 
 exports.createArticle = validate([
   body('article.title').notEmpty().withMessage('标题不能为空'),
   body('article.description').notEmpty().withMessage('描述不能为空'),
   body('article.body').notEmpty().withMessage('内容不能为空'),
+])
+
+exports.articleId = validate([
+  param('slug').custom(async val => {
+    if(!mongoose.isValidObjectId(val)) {
+      return Promise.reject('请传入正确文章id！')
+    }
+  })
 ])
