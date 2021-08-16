99fbde1ad52546f125e57c362b8494c778b462d1
邓威 <1536698087@qq.com>
2021-08-16 22:38:55 +0800

    更新文章验证是否有权限，id是否有效

--- validate/articles-v.js
+++ validate/articles-v.js
@@ -1,6 +1,7 @@
 const validate = require('../middleware/validate')
 const { body, param } = require('express-validator')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+const { articleModel } = require('../model')
 
 exports.createArticle = validate([
   body('article.title').notEmpty().withMessage('标题不能为空'),
@@ -9,9 +10,32 @@ exports.createArticle = validate([
 ])
 
 exports.articleId = validate([
-  param('slug').custom(async val => {
-    if(!mongoose.isValidObjectId(val)) {
-      return Promise.reject('请传入正确文章id！')
-    }
-  })
+  validate.checkArticleID(['params'], 'slug')
 ])
+
+exports.updateArticle = [
+  validate([
+    validate.checkArticleID(['params'], 'slug')
+  ]),
+  /**
+   * 判断文章是否存在
+   * 判断当前用户是否有权修改
+   */
+  async (req, res, next) => {
+    const articleId = req.params.slug
+    const article = await articleModel.findById(articleId)
+    if(!article) {
+      return res.status(404).send('没有该文章数据')
+    }
+    req.article = article
+    next()
+  },
+  async (req, res, next) => {
+    if(req.user._id.toString() !== req.article.author.toString()) {
+      return res.status(403).send('你没有权限修改此文章！！！')
+    }
+    next()
+  }
+]
+
+exports.deleteArticle = exports.updateArticle
